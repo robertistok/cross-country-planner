0620f374365f1314f5e32d7acab272384ead4d8d
robertistok <robertistok94@gmail.com>
2019-10-29 18:45:05 +0800

    Move out components into separate files

--- src/App.js
+++ src/App.js
@@ -1,79 +1,26 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import styled from "styled-components";
-import L from "leaflet";
 
-function App() {
-  // const [map, setMap] = useState();
-  const [map, setMap] = useState();
-  const [markers, setMarkers] = useState([]);
+import { AppStateProvider } from "./components/AppState";
+import WaypointsList from "./components/WaypointsList";
+import Map from "./components/Map";
 
-  useEffect(() => {
-    if (!map) {
-      const mapInstance = new L.map("map").setView([46.3342, 13.8287], 13);
-
-      new L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(mapInstance);
-
-      setMap(mapInstance);
-    }
-  }, [map]);
-
-  const addMarker = useCallback(
-    ev => {
-      const { lat, lng } = map.mouseEventToLatLng(ev.originalEvent);
-      L.marker([lat, lng], {
-        icon: new L.DivIcon({
-          className: "custom-pin",
-          html: `<span>${markers.length + 1}</span>`
-        })
-      }).addTo(map);
-      setMarkers(prev => [...prev, { lat, lng }]);
-    },
-    [map, markers.length]
-  );
-
-  useEffect(() => {
-    if (markers.length >= 2) {
-      new L.polyline(markers, { color: "#1b57d1" }).addTo(map);
-    }
-  });
-
-  useEffect(() => {
-    if (map) {
-      map.on("click", addMarker);
-    }
-
-    return () => {
-      if (map) {
-        map.off("click");
-      }
-    };
-  });
-
-  console.log(map);
-
+const App = () => {
   return (
-    <div className="App">
-      <MapContainer id="map"></MapContainer>
-    </div>
+    <AppStateProvider>
+      <Root>
+        <WaypointsList />
+        <Map />
+      </Root>
+    </AppStateProvider>
   );
-}
+};
 
-const MapContainer = styled.div`
-  height: 500px;
-
-  .custom-pin {
-    background-color: #000;
-    color: #fff;
-    border-radius: 50%;
-    height: 24px !important;
-    width: 24px !important;
-    font-size: 14px;
-    font-weight: 600;
-    text-align: center;
-  }
+const Root = styled.main`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-template-rows: 100%;
 `;
 
 export default App;
