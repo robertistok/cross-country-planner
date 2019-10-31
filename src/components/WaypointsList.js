9bcfa2610ec6365d20e504511ede77b7cc66401d
robertistok <robertistok94@gmail.com>
2019-10-31 09:12:42 +0800

    Add download route as gpx button

--- src/components/WaypointsList.js
+++ src/components/WaypointsList.js
@@ -56,6 +56,7 @@ const WaypointsList = () => {
           />
         ))}
       </WaypointsContainer>
+      <DownloadButton>Download your Route</DownloadButton>
     </Root>
   );
 };
@@ -63,20 +64,44 @@ const WaypointsList = () => {
 const Root = styled.section`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   background-color: #383838
   color: #ffffff;
-  overflow: auto;
   padding: 20px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+  position: relative;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: -100%;
+    left: 0;
+    right: 0;
+    height: 0.5em;
+    border-top: 4px solid #616161;
+  }
 `;
 
 const WaypointsContainer = styled.ul`
   display: flex;
   flex-direction: column;
   padding: 0;
+  height: 500px;
+  overflow: auto;
 `;
 
-const Title = styled.h1`
-  margin-bottom: 50px;
+const DownloadButton = styled.button`
+  color: #000;
+  font-weight: 700;
+  font-size: 14px;
+  padding: 10px 0px;
+  background-color: #c3e451;
+  width: 100%;
+  border: none;
+  border-radius: 2px;
 `;
 
 export default WaypointsList;
